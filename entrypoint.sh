664a6682721907f22173d5616929759557657ef4
Alex <me@djoh.xyz>
2022-08-26 23:47:20 +0000

    output node version on server startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@ sleep 1
 
 clear
 
+NODE_VERSION=$(node -v)
+echo "Node.JS version: $NODE_VERSION"
+
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
